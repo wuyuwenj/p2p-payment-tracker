2d7f356af67c58fdc0ed23c40cc6be7ac63d96d9
Jameswu <wuyuwen0@gmail.com>
2025-12-05 17:00:32 -0800

    Use @ts-ignore instead of @ts-expect-error for cross-env compatibility

--- components/AuthProvider.tsx
+++ components/AuthProvider.tsx
@@ -4,6 +4,6 @@ import { SessionProvider } from 'next-auth/react'
 import { ReactNode } from 'react'
 
 export default function AuthProvider({ children }: { children: ReactNode }) {
-  // @ts-expect-error - React 19 type compatibility issue with next-auth
+  // @ts-ignore - React 19 type compatibility issue with next-auth
   return <SessionProvider>{children}</SessionProvider>
 }
